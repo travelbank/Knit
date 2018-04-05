0d9bc0bef3f5aae9dc7ae4992016caa58fee39e4
OmerUygurOzer <omer@travelbank.com>
2018-04-05 15:04:47 -0700

    more docs

--- knitlib/src/main/java/com/omerozer/knit/schedulers/ImmediateScheduler.java
+++ knitlib/src/main/java/com/omerozer/knit/schedulers/ImmediateScheduler.java
@@ -24,6 +24,11 @@ public class ImmediateScheduler implements SchedulerInterface {
         this.isDone = new AtomicBoolean(false);
     }
 
+    /**
+     * Handles the callable immediately and passes the result to {@link Consumer}
+     * @param callable {@link Callable} task that's being passed.
+     * @param <T> Type of that the callable returns.
+     */
     @Override
     public <T> void submit(Callable<T> callable) {
         try {
@@ -44,28 +49,50 @@ public class ImmediateScheduler implements SchedulerInterface {
         }
     }
 
+    /**
+     * Handles {@link Runnable} immediately.
+     * @param runnable {@link Runnable} task that's being passed.
+     */
     @Override
     public void submit(Runnable runnable) {
         runnable.run();
         this.isDone.set(true);
     }
 
+    /**
+     * Starts the scheduler. Also registers in to the {@link com.omerozer.knit.schedulers.EvictorThread}
+     */
     @Override
     public void start() {
         EVICTOR_THREAD.registerScheduler(this);
     }
 
+
+    /**
+     * This does nothing as this Scheduler doesn't hold any other dependencies.
+     */
     @Override
     public void shutDown() {
 
     }
 
+
+    /**
+     * Sets the target {@link SchedulerInterface} which will run the consume task.
+     * @param schedulerInterface Target scheduler to run the consume task.
+     * @param consumer {@link Consumer} object that contains the way consume will occur.
+     * @param <T> Type that the consumer will receive.
+     */
     @Override
     public <T> void setTargetAndConsumer(SchedulerInterface schedulerInterface, Consumer consumer) {
         this.target.set(schedulerInterface);
         this.resultConsumer.set(consumer);
     }
 
+    /**
+     * If the task currently being executed is done ,it returns {@code true}. Otherwise {@code false}.
+     * @return whether or not the task is done.
+     */
     @Override
     public boolean isDone() {
         return isDone.get();
