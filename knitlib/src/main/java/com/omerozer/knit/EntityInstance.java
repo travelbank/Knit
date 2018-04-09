706628b1ebad7ea72dd3ad66779b11c0de66b97c
OmerUygurOzer <omer@travelbank.com>
2018-04-09 13:32:06 -0700

    more docs

--- knitlib/src/main/java/com/omerozer/knit/EntityInstance.java
+++ knitlib/src/main/java/com/omerozer/knit/EntityInstance.java
@@ -3,36 +3,67 @@ package com.omerozer.knit;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * Created by omerozer on 4/4/18.
+ * Container class for all {@link MemoryEntity} instances registered to {@link com.omerozer.knit.components.graph.UsageGraph}.
+ * Provides atomicity when nullifying an instance as it's being used by a Thread.
+ *
+ * @param <T> type of the {@link MemoryEntity} that is being stored.
  */
 
 public class EntityInstance<T extends MemoryEntity> {
 
+    /**
+     * {@link AtomicReference} that holds the {@link MemoryEntity}
+     */
     private AtomicReference<T> reference;
 
+    /**
+     * {@link Class} type of the entitiy being held.
+     */
     private Class<? extends MemoryEntity> clazz;
 
     public EntityInstance(){
         this.reference = new AtomicReference<>();
     }
 
+    /**
+     * Nullifies the instance stored in {@link this#reference}.
+     */
     public void nullify(){
         this.reference.set(null);
     }
 
+    /**
+     * Returns whether {@link this#reference} contains a null object.
+     * @return whether {@link this#reference} contains a null object.
+     */
     public boolean isAvailable(){
         return this.reference.get()!=null;
     }
 
+    /**
+     * Returns the instance contained by {@link this#reference}
+     * @return
+     */
     public T get() {
         return reference.get();
     }
 
+    /**
+     * Sets the instance contained inside {@link this#reference} and the class type of {@link this#clazz}.
+     * @param memoryEntity
+     */
     public void set(T memoryEntity){
         this.reference.set(memoryEntity);
         this.clazz = memoryEntity.getClass();
     }
 
+    /**
+     * Compares {@link this#clazz} to the given class.
+     * Used inside generated {@link InternalModel}s to determine which {@link com.omerozer.knit.generators.ValueGenerator} needs to be accessed
+     *
+     * @param inc given class to compare {@link this#clazz} to.
+     * @return Whether the class types match or not.
+     */
     public boolean instanceOf(Class<?> inc){
         return clazz.equals(inc);
     }
--- knitlib/src/main/java/com/omerozer/knit/ViewToPresenterMapInterface.java
+++ knitlib/src/main/java/com/omerozer/knit/ViewToPresenterMapInterface.java
@@ -3,12 +3,42 @@ package com.omerozer.knit;
 import java.util.List;
 
 /**
- * Created by omerozer on 2/6/18.
+ *
+ * When KnitProcessor scans classpath, it creates a class called {@code ViewToPresenterMap} that implements {@link ViewToPresenterMapInterface}.
+ * This class contains all data such as which presenter requires which data and what their parent classes are. Also maps all views to their {@link KnitPresenter}s.
+ * This is how {@link com.omerozer.knit.components.graph.UsageGraph} determines dependencies.
+ * This class is loaded via {@link com.omerozer.knit.classloaders.KnitUtilsLoader}.
+ *
+ * @author Omer Ozer
  */
 
+
 public interface ViewToPresenterMapInterface {
+
+    /**
+     * Returns which {@link KnitPresenter} the given view should be attached to.
+     * @param viewClass given view class.
+     * @return Class of the {@link KnitPresenter}.
+     */
     Class getPresenterClassForView(Class viewClass);
+
+    /**
+     * Returns which {@link InternalPresenter} given {@link KnitPresenter} attaches to.
+     * @param parentClass Given {@link KnitPresenter} class.
+     * @return Class of the corresponding {@link InternalPresenter}.
+     */
     Class getPresenterClassForPresenter(Class parentClass);
+
+    /**
+     * Returns all view class annotated with {@link KnitView}.
+     * @return A list of all classes annotated with {@link KnitView}.
+     */
     List<Class<?>> getAllViews();
+
+    /**
+     * Returns a list of all data tags that a given {@link KnitPresenter} has it's {@link ModelEvent} methods tagged with.
+     * @param clazz Given {@link KnitPresenter} class.
+     * @return A list of all String tags given {@link KnitPresenter} has it's {@link ModelEvent} methods tagged with.
+     */
     List<String> getUpdatingValues(Class clazz);
 }
